d12a2716f5d6fee312a37178e5c4bfcbce7e04ac
josevegara04 <jbvegar@eafit.edu.co>
2026-03-01 18:03:37 -0500

    <feat>: add Register class

--- BACKEND/Authentication/views.py
+++ BACKEND/Authentication/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+import json
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@method_decorator(csrf_exempt, name="dispatch")
+class RegisterView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            print("Datos recibidos en /register:")
+            print(data)
+
+            return JsonResponse({
+                "message": "Datos recibidos correctamente"
+            }, status=200)
+
+        except Exception as e:
+            print("Error al procesar la solicitud:", e)
+            return JsonResponse({
+                "error": "Error al procesar la solicitud"
+            }, status=400)
