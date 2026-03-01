2c37be35009c13171e07fbd63cd1929dddcc63b1
josevegara04 <jbvegar@eafit.edu.co>
2026-03-01 18:04:01 -0500

    <feat>: add cors

--- BACKEND/WorkNexus/settings.py
+++ BACKEND/WorkNexus/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "Authentication",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'WorkNexus.urls'
@@ -118,3 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = "Authentication.User"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
