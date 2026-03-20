14033b3ac51cdb8dd6df6948036ebdb2894dd134
josevegara04 <jbvegar@eafit.edu.co>
2026-03-20 06:29:19 -0500

    <fix>: fix footer double apereance

--- FRONTEND/src/pages/Index.tsx
+++ FRONTEND/src/pages/Index.tsx
@@ -1,14 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
-import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
     <>
       <Hero />
       <HowItWorks />
-      <Footer />
     </>
   );
 };
