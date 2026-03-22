339e5ef38f572e983398f0fddf83dcd7e470050e
josevegara04 <jbvegar@eafit.edu.co>
2026-03-22 14:19:20 -0500

    <feat>: craete CategoryCountView class

--- BACKEND/Services/views.py
+++ BACKEND/Services/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from .models import Service
 from .serializers import ServiceSerializer
+from django.db.models import Count
 
 class ServiceWithFreelancerSerializer(serializers.ModelSerializer):
     freelancer_name = serializers.CharField(source="freelancer.user.username")
@@ -23,3 +24,16 @@ class ServiceListView(APIView):
 
         serializer = ServiceWithFreelancerSerializer(services, many=True)
         return Response(serializer.data)
+
+class CategoryCountView(APIView):
+    def get(self, request):
+        data = (
+            Service.objects
+            .values("category")
+            .annotate(count=Count("id"))
+        )
+
+        # Convert to simpler format
+        result = {item["category"]: item["count"] for item in data}
+
+        return Response(result)
