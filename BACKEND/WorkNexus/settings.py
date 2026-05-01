375886134a32dba48822865f988a2204c0cab86b
josevegara04 <jbvegar@eafit.edu.co>
2026-05-01 10:22:57 -0500

    feat: add api keys

--- BACKEND/WorkNexus/settings.py
+++ BACKEND/WorkNexus/settings.py
@@ -11,7 +11,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -46,6 +54,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "Messaging",
     "corsheaders",
+    "payments",
 ]
 
 MIDDLEWARE = [
