dc4b7d7131897637f53c8b5b42d4ec0acb5bc1b8
josevegara04 <jbvegar@eafit.edu.co>
2026-05-01 11:51:02 -0500

    feat: add new fields to improve service information

--- BACKEND/Services/models.py
+++ BACKEND/Services/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from Authentication.models import FreelancerProfile
+from django.core.validators import MinValueValidator
+from django.utils import timezone
 
 class Service(models.Model):
     CATEGORY_CHOICES = [
@@ -13,6 +15,13 @@ class Service(models.Model):
     description = models.TextField()
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     freelancer = models.ForeignKey(FreelancerProfile, on_delete=models.CASCADE, related_name="services")
+    
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)], default=1000)
+    delivery_time = models.PositiveIntegerField(help_text="Días de entrega", default=7)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return f"{self.title} - {self.freelancer.user.username}"
